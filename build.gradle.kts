fda960138910ae6918d3b9e3762081d84e27d93b
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2023-05-11 19:46:47 +0200

    Initial commit - project setup

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+
+plugins {
+    kotlin("jvm") version "1.8.21"
+    id("io.ktor.plugin") version "2.3.0"
+}
+
+group = "dreamsoftware.com"
+version = "0.0.1"
+application {
+    mainClass.set("dreamsoftware.com.ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-auth-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-auth-jwt-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-call-logging-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-call-id-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-openapi:$ktor_version")
+    implementation("io.ktor:ktor-server-swagger:$ktor_version")
+    implementation("io.ktor:ktor-server-conditional-headers-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-compression-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-caching-headers-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
---
+++ src/main/kotlin/dreamsoftware/com/plugins/HTTP.kt
@@ -0,0 +1,38 @@
+package dreamsoftware.com.plugins
+
+import io.ktor.server.plugins.openapi.*
+import io.ktor.server.routing.*
+import io.ktor.server.plugins.swagger.*
+import io.ktor.server.plugins.conditionalheaders.*
+import io.ktor.server.plugins.compression.*
+import io.ktor.server.plugins.cachingheaders.*
+import io.ktor.http.content.*
+import io.ktor.http.*
+import io.ktor.server.application.*
+
+fun Application.configureHTTP() {
+    routing {
+        openAPI(path = "openapi")
+    }
+    routing {
+        swaggerUI(path = "openapi")
+    }
+    install(ConditionalHeaders)
+    install(Compression) {
+        gzip {
+            priority = 1.0
+        }
+        deflate {
+            priority = 10.0
+            minimumSize(1024) // condition
+        }
+    }
+    install(CachingHeaders) {
+        options { call, outgoingContent ->
+            when (outgoingContent.contentType?.withoutParameters()) {
+                ContentType.Text.CSS -> CachingOptions(CacheControl.MaxAge(maxAgeSeconds = 24 * 60 * 60))
+                else -> null
+            }
+        }
+    }
+}
