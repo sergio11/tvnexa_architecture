846c5949a31a6052c9bb4fb215cb7e5fc84f00e2
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2023-05-11 20:40:53 +0200

    Implement initial docker deployment file and create rake file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,16 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val quartz_version: String by project
 
 plugins {
     kotlin("jvm") version "1.8.21"
     id("io.ktor.plugin") version "2.3.0"
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
 }
 
 group = "dreamsoftware.com"
@@ -14,6 +20,16 @@ application {
 
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+ktor {
+    fatJar {
+        archiveFileName.set("iptv_service.jar")
+    }
+}
+
+tasks.compileKotlin {
+    kotlinOptions.jvmTarget = "11"
 }
 
 repositories {
@@ -34,6 +50,7 @@ dependencies {
     implementation("io.ktor:ktor-server-caching-headers-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("org.quartz-scheduler:quartz:$quartz_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
