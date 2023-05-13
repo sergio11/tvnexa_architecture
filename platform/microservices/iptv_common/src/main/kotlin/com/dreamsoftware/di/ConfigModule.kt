bc4f37827b362c48ef757069e8996a0739bb4d8b
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2023-05-13 12:18:49 +0200

    Implement common koin config module

---
+++ platform/microservices/iptv_common/src/main/kotlin/com/dreamsoftware/di/ConfigModule.kt
@@ -0,0 +1,19 @@
+package com.dreamsoftware.di
+
+import com.dreamsoftware.model.AppConfig
+import com.dreamsoftware.service.IFtpService
+import com.sksamuel.hoplite.ConfigLoaderBuilder
+import com.sksamuel.hoplite.addResourceOrFileSource
+import org.koin.dsl.module
+
+val configModule = module {
+    includes(ftpModule)
+    single<AppConfig> {
+        ConfigLoaderBuilder.default()
+            .addResourceOrFileSource(get<IFtpService>().getFile(fileName = "application", fileExt = "yml"))
+            .strict()
+            .build()
+            .loadConfigOrThrow()
+    }
+    single { get<AppConfig>().dbConfig }
+}
---
+++ platform/microservices/iptv_common/src/main/kotlin/com/dreamsoftware/service/IFtpService.kt
@@ -0,0 +1,5 @@
+package com.dreamsoftware.service
+
+interface IFtpService {
+    fun getFile(fileName: String, fileExt: String): String
+}
