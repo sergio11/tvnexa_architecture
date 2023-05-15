a0abe2111eb1419999294707bdb7d73c7bf7910e
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2023-05-15 13:20:13 +0200

    Implement support save operations across all database datasources

--- platform/microservices/iptv_common/src/main/kotlin/com/dreamsoftware/data/database/datasource/category/impl/CategoryDatabaseDataSourceImpl.kt
+++ platform/microservices/iptv_common/src/main/kotlin/com/dreamsoftware/data/database/datasource/category/impl/CategoryDatabaseDataSourceImpl.kt
@@ -7,8 +7,16 @@ import com.dreamsoftware.data.database.datasource.core.SupportDatabaseDataSource
 import com.dreamsoftware.data.database.entity.CategoryEntity
 import com.dreamsoftware.data.database.entity.CategoryTable
 import org.jetbrains.exposed.sql.ResultRow
+import org.jetbrains.exposed.sql.statements.UpdateBuilder
 
 internal class CategoryDatabaseDataSourceImpl(
     database: IDatabaseFactory,
     mapper: IOneSideMapper<ResultRow, CategoryEntity>
-): SupportDatabaseDataSource<CategoryEntity, String>(database, mapper, CategoryTable), ICategoryDatabaseDataSource
+): SupportDatabaseDataSource<CategoryEntity, String>(database, mapper, CategoryTable), ICategoryDatabaseDataSource {
+
+    override fun UpdateBuilder<Int>.onMapEntityToSave(entity: CategoryEntity) = with(entity) {
+        this@onMapEntityToSave[CategoryTable.name] = name
+        this@onMapEntityToSave[CategoryTable.categoryId] = id
+    }
+
+}
