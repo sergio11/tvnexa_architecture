cbae86794251ae2c742b771d0b4da7ebaa539475
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2023-12-30 10:56:02 +0100

    Implement several improvements at api services

--- platform/microservices/iptv_api_service/src/main/kotlin/com/dreamsoftware/api/services/impl/RegionServiceImpl.kt
+++ platform/microservices/iptv_api_service/src/main/kotlin/com/dreamsoftware/api/services/impl/RegionServiceImpl.kt
@@ -8,19 +8,24 @@ import com.dreamsoftware.core.ISimpleMapper
 import com.dreamsoftware.data.database.entity.RegionEntity
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import org.slf4j.LoggerFactory
 
 class RegionServiceImpl(
     private val regionRepository: IRegionRepository,
     private val regionMapper: ISimpleMapper<RegionEntity, RegionResponseDTO>
 ) : IRegionService {
 
+    private val log = LoggerFactory.getLogger(this::class.java)
+
     @Throws(AppException.InternalServerError::class)
     override suspend fun findAll(): Iterable<RegionResponseDTO> = withContext(Dispatchers.IO) {
         try {
-            val regions = regionRepository.findAll()
-            regions.map(regionMapper::map)
+            regionRepository
+                .findAll()
+                .map(regionMapper::map)
         } catch (e: Exception) {
-            throw AppException.InternalServerError(e.message ?: "Unknown error")
+            log.debug("RES (findAll) An exception occurred: ${e.message ?: "Unknown error"}")
+            throw AppException.InternalServerError("An error occurred while fetching all regions.")
         }
     }
 
@@ -30,14 +35,12 @@ class RegionServiceImpl(
     )
     override suspend fun findByCode(code: String): RegionResponseDTO = withContext(Dispatchers.IO) {
         try {
-            val region = regionRepository.findByCode(code)
-            if (region != null) {
-                regionMapper.map(region)
-            } else {
-                throw AppException.NotFoundException.RegionNotFoundException(code)
+            regionRepository.findByCode(code)?.let(regionMapper::map) ?: run {
+                throw AppException.NotFoundException.RegionNotFoundException("Region with code '$code' not found.")
             }
         } catch (e: Exception) {
-            throw AppException.InternalServerError(e.message ?: "Unknown error")
+            log.debug("RES (findByCode) An exception occurred: ${e.message ?: "Unknown error"}")
+            throw AppException.InternalServerError("An error occurred while finding region by code.")
         }
     }
 }
