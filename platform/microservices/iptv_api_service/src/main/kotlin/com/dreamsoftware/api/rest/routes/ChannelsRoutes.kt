ef287de350d0473a7dacf832df5fdffbd41d4fec
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2024-01-12 20:28:52 +0100

    Adding documentation to APIs

--- platform/microservices/iptv_api_service/src/main/kotlin/com/dreamsoftware/api/rest/routes/ChannelsRoutes.kt
+++ platform/microservices/iptv_api_service/src/main/kotlin/com/dreamsoftware/api/rest/routes/ChannelsRoutes.kt
@@ -11,11 +11,34 @@ import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import org.koin.ktor.ext.inject
 
+/**
+ * Class representing the routes related to channels in the application.
+ * These routes include functionalities such as retrieving channels based on category, country, and pagination,
+ * as well as finding a channel by its ID.
+ *
+ * @property channelService An instance of the [IChannelService] interface for handling channel-related operations.
+ */
 fun Route.channelRoutes() {
     val channelService by inject<IChannelService>()
 
+    /**
+     * Defines the routes under the "/channels" endpoint for channel-related operations.
+     */
     route("/channels") {
-        // Endpoint to retrieve all channels
+
+        /**
+         * Endpoint for retrieving channels based on category, country, and pagination.
+         * Accepts GET requests to "/channels/" and retrieves channels using parameters such as category, country,
+         * offset, and limit. Utilizes the [channelService.findByCategoryAndCountryPaginated] method.
+         * Generates a success response with a code of 2001, a message indicating successful channel retrieval,
+         * and data containing the paginated list of channels.
+         *
+         * Query Parameters:
+         * - category: The category of the channels.
+         * - country: The country of the channels.
+         * - offset: The offset for pagination (default is set to [DEFAULT_OFFSET]).
+         * - limit: The limit for the number of channels per page (default is set to [DEFAULT_PAGE_SIZE]).
+         */
         get("/") {
             with(call) {
                 val category = parameters["category"]
@@ -30,7 +53,16 @@ fun Route.channelRoutes() {
             }
         }
 
-        // Endpoint to retrieve a channel by its ID
+        /**
+         * Endpoint for finding a channel by its ID.
+         * Accepts GET requests to "/channels/{channelId}" and retrieves a channel by its ID
+         * using the [channelService.findById] method.
+         * Generates a success response with a code of 2002, a message indicating successful channel retrieval,
+         * and data containing the details of the found channel.
+         *
+         * Path Parameter:
+         * - channelId: The ID of the channel to be retrieved.
+         */
         get("/{channelId}") {
             with(call) {
                 parameters["channelId"]?.let { channelId ->
--- platform/microservices/iptv_api_service/src/main/kotlin/com/dreamsoftware/api/rest/routes/SubdivisionRoutes.kt
+++ platform/microservices/iptv_api_service/src/main/kotlin/com/dreamsoftware/api/rest/routes/SubdivisionRoutes.kt
@@ -7,11 +7,26 @@ import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import org.koin.ktor.ext.inject
 
+/**
+ * Class representing the routes related to subdivisions in the application.
+ * These routes include functionalities such as retrieving all subdivisions and finding a subdivision by its code.
+ *
+ * @property subdivisionService An instance of the [ISubdivisionService] interface for handling subdivision-related operations.
+ */
 fun Route.subdivisionRoutes() {
     val subdivisionService by inject<ISubdivisionService>()
 
+    /**
+     * Defines the routes under the "/subdivisions" endpoint for subdivision-related operations.
+     */
     route("/subdivisions") {
-        // Endpoint to retrieve all subdivisions
+
+        /**
+         * Endpoint for retrieving all subdivisions.
+         * Accepts GET requests to "/subdivisions/" and retrieves all subdivisions using the [subdivisionService.findAll] method.
+         * Generates a success response with a code of 6001, a message indicating successful retrieval of subdivisions,
+         * and data containing the list of subdivisions.
+         */
         get("/") {
             call.generateSuccessResponse(
                 code = 6001,
@@ -20,7 +35,16 @@ fun Route.subdivisionRoutes() {
             )
         }
 
-        // Endpoint to retrieve a subdivision by its code
+        /**
+         * Endpoint for finding a subdivision by its code.
+         * Accepts GET requests to "/subdivisions/{subdivisionCode}" and retrieves a subdivision by its code
+         * using the [subdivisionService.findByCode] method.
+         * Generates a success response with a code of 6002, a message indicating successful subdivision retrieval,
+         * and data containing the details of the found subdivision.
+         *
+         * Path Parameter:
+         * - subdivisionCode: The code of the subdivision to be retrieved.
+         */
         get("/{subdivisionCode}") {
             with(call) {
                 doIfParamExists("subdivisionCode") { regionCode ->
