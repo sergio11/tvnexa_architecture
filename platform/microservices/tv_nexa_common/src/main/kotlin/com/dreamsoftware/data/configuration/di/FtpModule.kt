24ccab050b63fb5f67063fcf6a02ae8c4dec2a15
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2024-02-22 20:35:23 +0100

    Using FTP hostname dynamic variable at Configuration datasource

--- platform/microservices/tv_nexa_common/src/main/kotlin/com/dreamsoftware/data/configuration/di/FtpModule.kt
+++ platform/microservices/tv_nexa_common/src/main/kotlin/com/dreamsoftware/data/configuration/di/FtpModule.kt
@@ -6,7 +6,7 @@ import com.dreamsoftware.data.configuration.model.FtpConfig
 import org.apache.commons.net.ftp.FTPClient
 import org.koin.dsl.module
 
-private const val FTP_HOSTNAME = "ftp_repository"
+private const val FTP_HOSTNAME = "192.168.1.39"
 private const val FTP_USER = "dev"
 private const val FTP_PASSWORD = "dev"
 private const val CONFIG_FILE_NAME = "application"
